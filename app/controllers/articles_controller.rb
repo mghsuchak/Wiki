6b9c8037d4e9fb147b1aea96c9c140ec45ebb9a4
Megha Suchak <megha@maulik-2.attlocal.net>
2015-11-08 18:29:56 -0800

    Add Articles with CRUD ability

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@articles=Article.all.order("created_at DESC")
+	end
+
+	def new
+		@article=Article.new
+	end
+
+	def create
+		@article=Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else	
+			render 'new'
+		end
+	end
+
+	def show	
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else	
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :content)
+	end
+
+	def find_article
+		@article=Article.find(params[:id])
+	end
+
+end
